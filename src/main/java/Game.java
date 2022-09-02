4109b1181df9359020c83ce5ec518e13ea7ca251
tom347 <tomfeldman2002@gmail.com>
2022-09-02 12:03:17 +0300

    added illegal scores checking

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -2,4 +2,12 @@ public class Game {
     public boolean isCompleted() {
         return false;
     }
+
+    public void roll(int score) {
+        if (score < 0) throw new IllegalScoreException();
+        if (score > 10) throw new IllegalScoreException();
+    }
+
+    public static class IllegalScoreException extends IllegalArgumentException {
+    }
 }
--- src/test/java/TestGame.java
+++ src/test/java/TestGame.java
@@ -1,10 +1,30 @@
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class TestGame {
+    private Game game;
+
+    @BeforeEach
+    void setUp() {
+        game = new Game();
+    }
+
     @Test
     void newGame_isNotCompleted() {
-        Game game = new Game();
-        Assertions.assertFalse(game.isCompleted());
+
+        assertFalse(game.isCompleted());
+    }
+
+    @Test
+    void rollWithNegativeValue_throwsIllegalScoreException() {
+        assertThrows(Game.IllegalScoreException.class, () -> game.roll(-1));
+    }
+
+    @Test
+    void rollWithValueLargerThanTen_throwIllegalScoreException() {
+        assertThrows(Game.IllegalScoreException.class, () -> game.roll(11));
     }
 }
