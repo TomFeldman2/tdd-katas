92ae5079fe137423fef6cb6936f23f2f91035cbb
tom347 <tomfeldman2002@gmail.com>
2022-09-02 11:33:16 +0300

    added game class

---
+++ src/test/java/TestGame.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+public class TestGame {
+    @Test
+    public void test() throws Exception {
+
+    }
+
+    @Test
+    void createGame() {
+        Game game = new Game();
+    }
+}
